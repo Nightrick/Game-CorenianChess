21624f5f6510d3bbbbeb90cdb655623a147f2c25
David <brushdm@gmail.com>
2022-06-25 15:45:04 -0400

    Added player classes for black and white.

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -9,12 +9,14 @@ import com.chess.engine.board.Move;
 
 public abstract class Piece {
 	
+	protected final PieceType pieceType;
 	protected final int piecePosition;
 	// pieceAlliance is to be treated as an enum, which is a special Java type used to define collections of constants. More precisely, a Java enum type is a special kind of Java class
 	protected final Alliance pieceAlliance;
 	protected final boolean isFirstMove;
 	
-	Piece(final int piecePosition, final Alliance pieceAlliance){
+	Piece(final PieceType pieceType, final int piecePosition, final Alliance pieceAlliance){
+		this.pieceType = pieceType;
 		this.pieceAlliance = pieceAlliance;
 		this.piecePosition = piecePosition;
 		this.isFirstMove = false;
@@ -32,27 +34,64 @@ public abstract class Piece {
 		return this.isFirstMove;
 	}
 	
+	public PieceType getPieceType() {
+		return this.pieceType;
+	}
+	
 	public abstract Collection<Move> calculateLegalMoves(final Board board);
 	
 	public enum PieceType {
 		
-		PAWN("P"),
-		KNIGHT("N"),
-		BISHOP("B"),
-		ROOK("R"),
-		QUEEN("Q"),
-		KING("K");
+		PAWN("P") {
+			@Override
+			public boolean isKing() {
+				return false;
+			}
+		},
+		KNIGHT("N") {
+			@Override
+			public boolean isKing() {
+				return false;
+			}
+		},
+		BISHOP("B") {
+			@Override
+			public boolean isKing() {
+				return false;
+			}
+		},
+		ROOK("R") {
+			@Override
+			public boolean isKing() {
+				return false;
+			}
+		},
+		QUEEN("Q") {
+			@Override
+			public boolean isKing() {
+				return false;
+			}
+		},
+		KING("K") {
+			@Override
+			public boolean isKing() {
+				return true;
+			}
+		};
 		
 		private String pieceName;
 		
 		PieceType(String pieceName){
 			this.pieceName = pieceName;
-		}
+			
+		} 
 		
 		@Override 
 		public String toString() {
 			return this.pieceName;
 		}	
+		
+		public abstract boolean isKing();
 	}
 
 }
