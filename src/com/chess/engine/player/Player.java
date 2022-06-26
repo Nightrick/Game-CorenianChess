67e50e35bdfe4ecd2453efc6001af69b34bb1f3b
David <brushdm@gmail.com>
2022-06-26 16:26:12 -0400

    Fleshed out player classes and added check/checkmate/stalemate logic.

--- src/com/chess/engine/player/Player.java
+++ src/com/chess/engine/player/Player.java
@@ -1,22 +1,38 @@
 package com.chess.engine.player;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
+import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
 import com.chess.engine.pieces.King;
 import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableList;
 
 public abstract class Player {
 	
 	protected final Board board;
 	protected final King playerKing;
 	protected final Collection<Move> legalMoves;
+	private final boolean isInCheck;
 	
 	Player(final Board board, final Collection<Move> legalMoves, final Collection<Move> opponentMoves){
 		this.board = board;
 		this.playerKing = establishKing();
 		this.legalMoves = legalMoves;
+		this.isInCheck = !Player.calculateAttacksOnTile(this.playerKing.getPiecePosition(), opponentMoves).isEmpty();
+	}
+
+	private static Collection<Move> calculateAttacksOnTile(int piecePosition, Collection<Move> moves) {
+		final List<Move> attackMoves = new ArrayList<>();
+		for(final Move move : moves) {
+			if(piecePosition == move.getDestinationCoordinate()) {
+				attackMoves.add(move);
+			}
+		}
+		return ImmutableList.copyOf(attackMoves);
 	}
 
 	protected King establishKing() {
@@ -25,9 +41,47 @@ public abstract class Player {
 				return (King) piece;
 			}
 		}
-		throw new RuntimeException("The game shouldn't reach here! This isn't a valid board.");
+		throw new RuntimeException("The game shouldn't Creach here! This isn't a valid board.");
+	}
+	
+	public boolean isMoveLegal(final Move move) {
+		return this.legalMoves.contains(move);
+	}
+	
+	//The three methods below require further implementation
+	public boolean isInCheck() {
+		return this.isInCheck;
+	}
+	
+	public boolean isInCheckMate() {
+		return this.isInCheck && !hasEscapeMoves();
+	}
+	
+	protected boolean hasEscapeMoves() {
+		
+		for(final Move move : this.legalMoves) {
+			final MoveTransition transition = makeMove(move);
+			if(transition.getMoveStatus().isDone()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean isInStaleMate() {
+		return !this.isInCheck && !hasEscapeMoves();
+	}
+	
+	public boolean isCastled() {
+		return false;
+	}
+	
+	public MoveTransition makeMove(final Move move) {
+		return null; 
 	}
 	
 	public abstract Collection<Piece> getActivePieces();
+	public abstract Alliance getAlliance();
+	public abstract Player getOpponent();
 
 }
